6465c9c0cca52d3fe85d3cb5828f43890a8cf4d2
daluclemas <chidaluanukam@gmail.com>
2024-06-17 09:57:31 +0100

    update: add a http redirect to https

--- src/App.js
+++ src/App.js
@@ -22,6 +22,16 @@ function App() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
+
+  useEffect(() => {
+    // Check if the current protocol is HTTP
+    if (process.env.NODE_ENV === "production") {
+      if (window.location.protocol === "http:") {
+        // Redirect to the equivalent URL with HTTPS
+        window.location.href = `https://${window.location.hostname}${window.location.pathname}`;
+      }
+    }
+  }, []);
   return (
     <QueryClientProvider client={queryClient}>
       <Routes>

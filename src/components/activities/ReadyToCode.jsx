308d94a96953de72cdd6903f252e3e2983ab2be3
HeemahMuslad <rohimat.m1800276@st.futminna.edu.ng>
2024-07-29 19:36:43 +0100

    update: removal of usestate on readytocode component

--- src/components/activities/ReadyToCode.jsx
+++ src/components/activities/ReadyToCode.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { FaAngleRight,} from "react-icons/fa";
 import { starImage } from "../../assets/images";

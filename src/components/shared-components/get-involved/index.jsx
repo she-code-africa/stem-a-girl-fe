34f5ce3d7adee146de894dc8d58884bb4e2448a0
Grace <grace.adelaka@charisol.io>
2026-03-04 20:42:02 +0100

    fix: update Get Involved card to contain both text and image within white background

--- src/components/shared-components/get-involved/index.jsx
+++ src/components/shared-components/get-involved/index.jsx
@@ -13,16 +13,16 @@ const GetInvolved = () => {
         style={{ backgroundImage: `url(${getInvolvedBg})` }}
       >
         <div className="w-[90%] max-w-[1280px] mx-auto">
-          <div className="bg-white rounded-2xl flex flex-col lg:flex-row overflow-hidden">
-            <div className="flex-1 p-8 md:p-10 lg:p-12 flex flex-col gap-5 justify-center">
+          <div className="bg-white rounded-2xl flex flex-col lg:flex-row p-6 md:p-8 gap-6 lg:gap-8 items-center">
+            <div className="flex-1 flex flex-col gap-5 justify-center">
               <h2 className="text-[#7D355D] font-semibold text-3xl md:text-4xl lg:text-5xl">
                 Get Involved
               </h2>
               <p className="text-black text-base md:text-lg font-normal">
                 As a student, teacher, or parent, help us inspire more African
-                girls to kick start their journey into STEM!
+                girls <br />to kick start their journey into STEM!
               </p>
-              <ul className="text-black font-normal border-l-4 border-[#B70569] pl-4 flex flex-col gap-3">
+              <ul className="text-black font-normal border-l-4 border-[#B70569] pl-4 flex flex-col gap-3 max-w-[379px]">
                 {bulletPoints.map((point, i) => (
                   <li key={i} className="text-black text-base md:text-lg">
                     {point}
@@ -38,11 +38,11 @@ const GetInvolved = () => {
                 </button>
               </div>
             </div>
-            <div className="w-full lg:w-[45%] flex-shrink-0 max-h-[400px] lg:max-h-none">
+            <div className="w-full lg:w-[45%] flex-shrink-0 h-[280px] lg:h-[360px]">
               <img
                 src={getInvolvedHeroImg}
                 alt="Get involved"
-                className="w-full h-full object-cover"
+                className="w-full h-full object-cover rounded-2xl"
               />
             </div>
           </div>

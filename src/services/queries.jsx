9284f6c8e2e59bfe9f9b44d261d5e7e931d02206
daluclemas <chidaluanukam@gmail.com>
2026-03-31 14:16:43 +0100

    update the query url

--- src/services/queries.jsx
+++ src/services/queries.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL = process.env.REACT_APP_API_URL;
 
 export const getAllActivities = async () => {
-  const activities = await axios.get(`${baseURL}activity`);
+  const activities = await axios.get(`${baseURL}/activity`);
   return activities.data.data;
 };
 
@@ -15,47 +15,47 @@ export const getReports = async () => {
 
 export const getActivityCourses = async (activityID) => {
   const activityCourses = await axios.get(
-    `${baseURL}course?activity=${activityID}`,
+    `${baseURL}/course?activity=${activityID}`,
   );
   return activityCourses.data.data;
 };
 
 export const getActivityEvents = async (activityID) => {
   const activityEvents = await axios.get(
-    `${baseURL}event?activity=${activityID}`,
+    `${baseURL}/event?activity=${activityID}`,
   );
   return activityEvents.data.data;
 };
 
 export const getAllTestimonials = async () => {
-  const testimonials = await axios.get(`${baseURL}testimonials`);
+  const testimonials = await axios.get(`${baseURL}/testimonials`);
   return testimonials.data.data;
 };
 
 export const getAllSchools = async () => {
-  const schools = await axios.get(`${baseURL}school`);
+  const schools = await axios.get(`${baseURL}/school`);
   return schools.data.data;
 };
 
 export const getAllImpactStories = async () => {
-  const impactStories = await axios.get(`${baseURL}impactStory`);
+  const impactStories = await axios.get(`${baseURL}/impactStory`);
 
   return impactStories.data.data;
 };
 
 // courses
 export const getAllCourses = async () => {
-  const courses = await axios.get(`${baseURL}course`);
+  const courses = await axios.get(`${baseURL}/course`);
 
   return courses.data.data;
 };
 
 export const getACourse = async (courseID) => {
-  const course = await axios.get(`${baseURL}course/${courseID}`);
+  const course = await axios.get(`${baseURL}/course/${courseID}`);
   return course.data.data;
 };
 //outreach
 export const getOutreach = async () => {
-  const outreach = await axios.get(`${baseURL}outreach`);
+  const outreach = await axios.get(`${baseURL}/outreach`);
   return outreach?.data?.data;
 };

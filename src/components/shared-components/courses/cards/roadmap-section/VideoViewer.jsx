eb508a616249c16c990aa0fb15b5c7ea61a4d61a
daluclemas <chidaluanukam@gmail.com>
2026-04-17 11:33:07 +0100

    update: change the url to youtube link

--- src/components/shared-components/courses/cards/roadmap-section/VideoViewer.jsx
+++ src/components/shared-components/courses/cards/roadmap-section/VideoViewer.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 
-
 const VideoViewer = ({ lesson }) => {
+  // const formatDrivePreviewLink = (url) => {
+  //   if (!url) return "";
 
- const formatDrivePreviewLink = (url) => {
-  if (!url) return "";
+  //   // Extract file ID from different possible formats
+  //   const match = url.match(/\/d\/([^/]+)/);
 
-  // Extract file ID from different possible formats
-  const match = url.match(/\/d\/([^/]+)/);
+  //   if (!match) return url; // fallback if not a drive link
 
-  if (!match) return url; // fallback if not a drive link
+  //   const fileId = match[1];
 
-  const fileId = match[1];
+  //   return `https://drive.google.com/file/d/${fileId}/preview`;
+  // };
 
-  return `https://drive.google.com/file/d/${fileId}/preview`;
-};
-  
-    
   return (
     <div className="w-full my-3">
       <iframe
         title={lesson.title}
-        src={formatDrivePreviewLink(lesson.videoUrl)}
+        src={lesson.videoUrl}
+        // src={formatDrivePreviewLink(lesson.videoUrl)}
+        // src={"https://www.youtube.com/embed/h71dBFr1wsY?si=AqxfWQQXz1hfSfbV"}
         width="100%"
         height="600px"
         allow="autoplay"
